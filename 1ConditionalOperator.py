2a81c86a06aae0f49f9dce9ee7d880ccd20413ff
blackshadowog <atiwari73874@gmail.com>
2026-06-23 10:53:21 +0530

    Add files via upload

---
+++ 1ConditionalOperator.py
@@ -0,0 +1,16 @@
+a=int(input("enter your age"))
+if(a>=18):
+    print("you are eligble for this")
+    print("here u go")
+
+elif(a==0):
+    print("are u making my fun bastard")
+
+elif(a<0):
+    print("take a brith fisrt u bakaga")
+
+else:
+    print("sorry you are not allowed now ")
+    print("come after some years")
+
+print("end this $h!t")

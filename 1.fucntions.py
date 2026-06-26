04aaaf10898f52e474e656a8799a267d5e26aa98
blackshadowog <atiwari73874@gmail.com>
2026-06-26 11:55:03 +0530

    Add files via upload

---
+++ 1.fucntions.py
@@ -0,0 +1,18 @@
+# funtion defenion
+def avg():
+    a = int(input("enter your number: "))
+    b = int(input("enter your number: "))
+    c = int(input("enter your number: "))
+    
+    avgrage = (a+b+c)/3
+    print(avgrage)
+#function call
+avg()
+avg()
+
+
+
+
+
+
+

be3486ed7f9474da9a4fb4b5f62f008753f9a3cd
blackshadowog <atiwari73874@gmail.com>
2026-06-26 11:55:41 +0530

    Add files via upload

---
+++ 3+functions+aeguments.py
@@ -0,0 +1,14 @@
+def fun1(name,ending):
+    print("good day" + name )
+    print(ending)
+
+fun1("HARRY","THANKS")
+
+def fun1(name,ending):
+    print("good day" + name )
+    print(ending)
+    return "IT WORKS"
+
+A = fun1("HARRY","THANKS")
+print(A)
+

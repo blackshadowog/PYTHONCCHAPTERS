4953d32d3ac718cf6d1884585ec70714f0c3611d
blackshadowog <atiwari73874@gmail.com>
2026-06-28 14:37:53 +0530

    Add files via upload

---
+++ 5-constructer.py
@@ -0,0 +1,21 @@
+class dudesinfo():
+
+    language = "py"
+    salary = " 250 million dollor "
+
+    def __init__(self):
+        print("abhishek ")
+
+    def getinfo(self):
+        print(f"your language is {self.language} and salary is {self.salary}")
+    @staticmethod
+    def greet():
+        print("good morning")
+
+abhishek = dudesinfo()
+abhishek.language= "java"
+
+# print(abhishek.language , abhishek.salary)
+abhishek.greet()
+# dudesinfo.getinfo(abhishek)
+abhishek.getinfo()
